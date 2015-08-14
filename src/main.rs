c84663031580975e857a645deb28dff99cc46e46
Cory Benfield <lukasaoz@gmail.com>
2015-08-14 16:20:17 -0400

    Handle basic command structure.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ extern crate postgres;
 extern crate chrono;
 extern crate byteorder;
 extern crate num;
+extern crate clap;
 
 mod currency;
 
 use postgres::{Connection, SslMode};
 use chrono::NaiveDateTime;
 use currency::Currency;
-
+use clap::{App, SubCommand, ArgMatches};
 
 // Define the data in the database.
 struct Account {
@@ -32,21 +33,61 @@ struct Deposit {
     amount: Currency
 }
 
-fn main() {
+fn prepare_interface<'a, 'b>() -> ArgMatches<'a, 'b> {
+    // Top level command.
+    let mut app = App::new("investments")
+                      .about("Keeps track of investments");
+
+    // Subcommands for the 'account' subcommand.
+    let mut accounts_sub = SubCommand::with_name("account")
+                                  .about("manage accounts");
+    let accounts_list = SubCommand::with_name("list")
+                                   .about("list accounts");
+    accounts_sub = accounts_sub.subcommand(accounts_list);
+
+    // Register top-level subcommands.
+    app = app.subcommand(accounts_sub);
+
+    return app.get_matches();
+}
+
+
+// List the accounts stored and their balances.
+fn list_accounts() {
     let conn = Connection::connect("postgres://cory@localhost:5432/finances", &SslMode::None).unwrap();
 
     let stmt = conn.prepare("
-        SELECT DISTINCT ON (balance.account) accounts.name, balance.balance
+        SELECT DISTINCT ON (balance.account) accounts.id, accounts.name, balance.balance
         FROM balance
         INNER JOIN accounts ON balance.account=accounts.id
         ORDER BY balance.account, balance.as_of DESC"
     ).unwrap();
 
-    println!("Balances:");
+    println!("Accounts:");
 
     for row in stmt.query(&[]).unwrap() {
-        let name: String = row.get(0);
-        let balance: Currency = row.get(1);
-        println!("\t{}: {}", name, balance);
+        let account_id: i32 = row.get(0);
+        let name: String = row.get(1);
+        let balance: Currency = row.get(2);
+        println!("\t({}) {}: {}", account_id, name, balance);
+    }
+}
+
+
+// Handle the account subcommand.
+fn handle_accounts(matches: &ArgMatches) {
+    match matches.subcommand() {
+        ("list", Some(matches)) => {list_accounts()},
+        _                       => {},
+    }
+}
+
+
+fn main() {
+    let matches = prepare_interface();
+
+    match matches.subcommand() {
+        ("account", Some(matches)) => {handle_accounts(matches)},
+        _                          => {},
     }
 }
